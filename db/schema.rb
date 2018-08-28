2c222e09008cb1d34519417d7db2804ee6f7f5df
しまりすk4 <neidhardt0209@gmail.com>
2018-08-28 09:47:52 +0900

    some error fixed

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2018_08_21_005859) do
 
-  create_table "dailies", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+
 
   create_table "diaries", force: :cascade do |t|
     t.string "title"
